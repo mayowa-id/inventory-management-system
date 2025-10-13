341ac152f5e1b9e17160c4a5a3e83d18ace064b0
Olumayowa Idowu <idowumayowa02@gmail.com>
2025-10-13 12:23:24 +0100

    Update app.js

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,13 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 import suppliersRouter from './routes/suppliers.routes.js';
 import warehousesRouter from './routes/warehouses.routes.js';
 import productsRouter from './routes/products.routes.js';
 import purchaseOrdersRouter from './routes/purchaseOrders.routes.js';
-import { sequelize } from './models/index.js';
 import reorderRoutes from './routes/reorder.routes.js';
+import { sequelize } from './models/index.js';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173', 
+    credentials: true, 
+  })
+);
+
 app.use(express.json());
 
 app.use('/api/v1/suppliers', suppliersRouter);
@@ -15,16 +24,6 @@ app.use('/api/v1/warehouses', warehousesRouter);
 app.use('/api/v1/products', productsRouter);
 app.use('/api/v1/purchase-orders', purchaseOrdersRouter);
 app.use('/api/v1/reorder', reorderRoutes);
-
-sequelize
-  .sync({ alter: true }) 
-  .then(() => {
-    console.log(' Database synchronized successfully');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => {
-    console.error(' Database sync failed:', err);
-  });
 
 app.get('/api/v1/health', async (req, res) => {
   try {
@@ -35,5 +34,15 @@ app.get('/api/v1/health', async (req, res) => {
     res.status(500).json({ status: 'error', message: err.message });
   }
 });
+
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log(' Database synchronized successfully');
+    app.listen(PORT, () => console.log(` Server running on http://localhost:${PORT}`));
+  })
+  .catch((err) => {
+    console.error(' Database sync failed:', err);
+  });
 
 export default app;
