800b3dc88ad24304a5a266381500e3fad76db53d
mayowa-id <idowumayowa02@gmail.com>
2025-12-09 11:55:52 +0100

    changes made to db config

--- BACKEND/src/config/db.js
+++ BACKEND/src/config/db.js
@@ -2,15 +2,20 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-
 export const sequelize = new Sequelize(
-process.env.DB_NAME,
-process.env.DB_USER,
-process.env.DB_PASS,
-{
-host: process.env.DB_HOST || 'localhost',
-port: process.env.DB_PORT || 5432,
-dialect: process.env.DB_DIALECT || 'postgres',
-logging: false
-}
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: process.env.NODE_ENV === 'production' ? {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    } : {}
+  }
 );
